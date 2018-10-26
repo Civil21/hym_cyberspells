834aacf7c4fab6ebc7082fb4e061af6caced4184
ymerenich1998 <ymerenich1998@gmail.com>
2018-10-27 00:15:19 +0300

    Add exit in index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181026180227) do
+ActiveRecord::Schema.define(version: 20181026201811) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20181026180227) do
   end
 
   create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quests", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
