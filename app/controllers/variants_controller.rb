f3f893f19f7ebce45d429a546e79e2600fa0fc10
Civil21 <diusolexander@gmail.com>
2018-10-27 09:24:55 +0300

    Hack your mind update variants

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -16,6 +16,11 @@ class VariantsController < ApplicationController
 
 	def create
 		@variant=Variant.create(variant_params)
+		if(params[:variant][:quest_sel]=='1')
+			Quest.find(params[:variant][:quest_id]).variants <<@variant
+		else
+			Variant.find(params[:variant][:variant_id]).variants <<@variant
+		end
   		if @variant.save
 			redirect_to root_path  		
     	else
@@ -39,8 +44,8 @@ class VariantsController < ApplicationController
 		@variant ||=Variant.find(params[:id])
 	end
 
-  	def item_params
-  		params.require(:variant).permit(:name,:description)
+  	def variant_params
+  		params.require(:variant).permit(:text,:description)
   	end
 
 end
